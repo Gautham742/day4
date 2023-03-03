bfcb4a4c21058e6cff533d37b2789b0f8cb82bc8
Gautham742 <gauthamk742@gmail.com>
2023-03-03 14:34:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DistrictpageComponent } from './districtpage/districtpage.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
+import { SigninpageComponent } from './signinpage/signinpage.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { FooterComponent } from './units/footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SinglepageComponent } from './singlepage/singlepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    DistrictpageComponent,
+    SignuppageComponent,
+    SigninpageComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    SinglepageComponent
   ],
   imports: [
     BrowserModule,
